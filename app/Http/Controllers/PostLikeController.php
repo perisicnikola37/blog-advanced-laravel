4913d3606fccc2f9e172abdf9010b30b9c5cf067
Nikola Perisic <5dzoni5@gmail.com>
2022-07-02 20:53:49 +0200

    Feature Initial Commit - finished Like/Unlike system

---
+++ app/Http/Controllers/PostLikeController.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Post;
+use Illuminate\Http\Request;
+
+class PostLikeController extends Controller
+{
+
+    public function __construct()
+    {
+        
+        $this->middleware(['auth']);
+
+    }
+    
+    public function store(Post $post, Request $request) {
+        
+
+      if ($post->likedBy($request->user())) {
+        return response(null, 409);
+      }
+    
+
+      $post->likes()->create([
+
+        'user_id' => auth()->user()->id,
+
+      ]);
+
+      return back();
+
+    }
+
+    public function destroy(Post $post, Request $request) {
+
+        $request->user()->likes()->where('post_id', $post->id)->delete();
+
+        return back();
+
+    }
+
+}
