b6483c58fb17f43bfa58e5dc9d22af47e076833e
Nikola Perisic <5dzoni5@gmail.com>
2022-07-05 17:22:01 +0200

    Changed migrations - added default values

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -2,6 +2,9 @@
 
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Carbon;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Hash;
 use Illuminate\Support\Facades\Schema;
 
 return new class extends Migration
@@ -23,6 +26,15 @@ return new class extends Migration
             $table->rememberToken();
             $table->timestamps();
         });
+
+        DB::table('users')->insert([
+            'name' => 'Test User',
+            'email' => 'test@gmail.com',
+            'password' => Hash::make('testblog'),
+            'admin' => 'true',
+            'created_at' => Carbon::now(),
+        ]);
+
     }
 
     /**
--- database/migrations/2022_07_04_214024_create_quotes_table.php
+++ database/migrations/2022_07_04_214024_create_quotes_table.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Schema;
 
 return new class extends Migration
@@ -15,11 +16,17 @@ return new class extends Migration
     {
         Schema::create('quotes', function (Blueprint $table) {
             $table->id();
-            $table->integer('user_id');
+            $table->integer('user_id')->default('0');
             $table->string('author');
             $table->string('body');
             $table->timestamps();
         });
+
+        DB::table('quotes')->insert([
+            'author' => 'Nikola Tesla',
+            'body' => "I don't care that they stole my idea . . I care that they don't have any of their own",
+        ]);
+
     }
 
     /**
