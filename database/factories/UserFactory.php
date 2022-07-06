dbd6951c529c6de33d145bd03a0b936768b4b7c3
Nikola Perisic <5dzoni5@gmail.com>
2022-07-06 15:42:46 +0200

    User Factory - added picture values

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -2,6 +2,7 @@
 
 namespace Database\Factories;
 
+use Carbon\Carbon;
 use Illuminate\Database\Eloquent\Factories\Factory;
 use Illuminate\Support\Str;
 
@@ -21,9 +22,13 @@ class UserFactory extends Factory
             'name' => fake()->name(),
             'username' => fake()->name(),
             'email' => fake()->safeEmail(),
-            'email_verified_at' => now(),
+            'email_verified_at' => Carbon::now(),
             'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi',
             'admin' => 'false',
+            'picture' => 'no-picture',
+            'random' => 'https://source.unsplash.com/random',
+            'created_at' => Carbon::now(),
+            'created_at' => Carbon::now(),
             'remember_token' => Str::random(10),
         ];
     }
